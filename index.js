c8487a74b639f54d1c1c25c9d7e3ef485358f417
Justin Liu <justinliu@Justins-MacBook-Pro.local>
2022-06-01 18:11:13 +0100

    Write out steps to take

---
+++ index.js
@@ -0,0 +1,6 @@
+// 1. Fetch input from txt file
+// 2. Refactor data for usability
+// 3. Init grid using input data
+// 4. Init robots and run
+// 5. Store robot location data
+// 6. Output data as string to terminal 
